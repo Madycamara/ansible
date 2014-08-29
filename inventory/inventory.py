57ba47582669b441096d6a63b78dd67e368214fe
ZUNbado <git@acs.li>
2014-08-29 17:25:12 +0200

    optimizing inventory.py script (fewer mysql querys, more speed)

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -9,8 +9,9 @@ try:
 except ImportError:
     import simplejson as json
 
-def grouplist(conn):
+def getgroups(conn):
     inventory ={ }
+    inventory['_meta']={ 'hostvars' : {} }
     inventory['all']= { 'hosts': [] }
     cur = conn.cursor()
     cur.execute("SELECT IF(group.name is null,'all',group.name) AS `group`,host.name FROM host LEFT JOIN host_group ON host_group.host_id=host.id LEFT JOIN `group` ON host_group.group_id=group.id")
@@ -25,64 +26,63 @@ def grouplist(conn):
 	if not name in inventory['all']['hosts']:
 	  inventory['all']['hosts'].append(name)
 
+    return inventory
 
-    for group in inventory:
-         result=retvars(conn,'group',group)
-         inventory[group]['vars']=result
-         roles=retroles(conn,'group',group)
-         if len(roles) > 0:
-           inventory[group]['vars']['roles_group']=roles
-
-    inventory['_meta']={ 'hostvars' : {} }
-    for host in inventory['all']['hosts']:
-      inventory['_meta']['hostvars'][host]=hostinfo(conn,host,False)
-
-    cur.close()
-    return json.dumps(inventory, indent=4)
-
-def hostinfo(conn, name, printout=True):
-  result=retvars(conn,'host',name)
-  roles=retroles(conn,'host',name)
-  result['roles']=roles
-  if printout == True:
-     print json.dumps(result, indent=4)
-  else:
-    return result
-
-def retroles(conn, type, name):
+def getroles(conn, type, inventory):
   result = {}
   cur = conn.cursor()
   if type == 'host':
-    cur.execute("SELECT role.name AS role FROM host_roles INNER JOIN role ON role.id=host_roles.role_id INNER JOIN host ON host.id=host_roles.host_id WHERE host.name=%s", name)
+    cur.execute("SELECT host.name,role.name AS role FROM host_roles INNER JOIN role ON role.id=host_roles.role_id INNER JOIN host ON host.id=host_roles.host_id")
   elif type == 'group':
-    cur.execute("SELECT role.name AS role FROM group_roles INNER JOIN role ON role.id=group_roles.role_id INNER JOIN `group` ON group.id=group_roles.group_id WHERE group.name=%s", name)
+    cur.execute("SELECT group.name,role.name AS role FROM group_roles INNER JOIN role ON role.id=group_roles.role_id INNER JOIN `group` ON group.id=group_roles.group_id")
 
   rows=cur.fetchall()
-  for row in rows:
-    result[row[0]]='true'
-    #result.append(row[0])
+  if type == 'group':
+     for row in rows:
+       if row[0] in inventory:
+         if not 'roles' in inventory[row[0]]:
+            inventory[row[0]]['roles']=[]
+         inventory[row[0]]['roles'].append(row[1])
 
-  return result
+  if type == 'host':
+     for row in rows:
+       if not row[0] in inventory['_meta']['hostvars']:
+         inventory['_meta']['hostvars'][row[0]]={}
+       if not 'roles' in inventory['_meta']['hostvars']:
+            inventory['_meta']['hostvars'][row[0]]['roles']=[]
+       inventory['_meta']['hostvars'][row[0]]['roles'].append(row[1])
 
-def retvars(conn, type, name):
+  return inventory
+
+def getvars(conn, type, inventory):
   result = {}
   cur = conn.cursor()
   if type == 'host':
-    cur.execute("SELECT single_var.name,value FROM host_vars INNER JOIN host ON host_id=host.id INNER JOIN single_var ON single_var_id=single_var.id WHERE host.name=%s", name)
+    cur.execute("SELECT host.name,single_var.name,value FROM host_vars INNER JOIN host ON host_id=host.id INNER JOIN single_var ON single_var_id=single_var.id")
+
   elif type == 'group':
-    cur.execute("SELECT single_var.name,value FROM group_vars INNER JOIN `group` ON group.id=group_vars.group_id INNER JOIN single_var ON single_var_id=single_var.id WHERE group.name=%s", name)
+    cur.execute("SELECT group.name,single_var.name,value FROM group_vars INNER JOIN `group` ON group.id=group_vars.group_id INNER JOIN single_var ON single_var_id=single_var.id")
 
   rows=cur.fetchall()
-  for row in rows:
-    result[row[0]]=row[1]
+  if type == 'group':
+    for row in rows:
+       if row[0] in inventory:
+         if not 'vars' in inventory[row[0]]:
+           inventory[row[0]]['vars']={}
+         inventory[row[0]]['vars'][row[1]]=row[2]
+  if type == 'host':
+     for row in rows:
+       if not row[0] in inventory['_meta']['hostvars']:
+         inventory['_meta']['hostvars'][row[0]]={}
+       inventory['_meta']['hostvars'][row[0]][row[1]]=row[2]
 
   cur.execute("SELECT tablename,env FROM roletable")
   tables=cur.fetchall()
   for table in tables:
     if type == 'host':
-      cur.execute('SELECT ' + table[0] + '.* FROM ' + table[0] + ' INNER join host ON host.id=host_id WHERE host.name=%s', name)
+      cur.execute('SELECT host.name,' + table[0] + '.* FROM ' + table[0] + ' INNER join host ON host.id=host_id')
     elif type == 'group':
-      cur.execute('SELECT ' + table[0] + '.* FROM ' + table[0] + ' INNER join `group` ON group.id=group_id WHERE group.name=%s',name)
+      cur.execute('SELECT group.name,' + table[0] + '.* FROM ' + table[0] + ' INNER join `group` ON group.id=group_id')
 
     if type == 'group' and table[1] == 'multi':
       tablespace=table[0]+'_global'
@@ -90,23 +90,50 @@ def retvars(conn, type, name):
       tablespace=table[0]
 
     rows=cur.fetchall()
-    if len(rows) > 0:
-      result[tablespace]=[]
     columns=cur.description
     for row in rows:
       tmp={}
       for (index,column) in enumerate(row):
-        if index >= 3 and column != None:
+        if index >= 4 and column != None:
          tmp[columns[index][0]] = column
-      result[tablespace].append(tmp)
+      if type == 'group':
+        if row[0] in inventory:
+          if not 'vars' in inventory[row[0]]:
+           inventory[row[0]]['vars']={}
+          if not tablespace in inventory[row[0]]['vars']:
+            inventory[row[0]]['vars'][tablespace]=[]
+          inventory[row[0]]['vars'][tablespace].append(tmp)
+      if type == 'host':
+        if not row[0] in inventory['_meta']['hostvars']:
+          inventory['_meta']['hostvars'][row[0]]={}
+        if not tablespace in inventory['_meta']['hostvars'][row[0]]:
+          inventory['_meta']['hostvars'][row[0]][tablespace]=[]
+        inventory['_meta']['hostvars'][row[0]][tablespace].append(tmp)
     
-  return result
+  return inventory
 
-def writecache(con,tmpfile):
-	json=grouplist(con)
-	cache=open(tmpfile, 'w')
-	cache.write(json)
-	cache.close()
+
+def getjson(config):
+        try:
+                con = mdb.connect(config.get('ansible','host'), config.get('ansible','user'), config.get('ansible','password'), config.get('ansible','db'));
+        except mdb.Error, e:
+                print "Error %d: %s" % (e.args[0],e.args[1])
+                sys.exit(1)
+
+        inventory=getgroups(con)
+        inventory=getvars(con,'group',inventory)
+        inventory=getroles(con,'group',inventory)
+        inventory=getroles(con,'host',inventory)
+        inventory=getvars(con,'host',inventory)
+        con.close()
+        return inventory
+
+
+def writecache(config,tmpfile):
+  inventory=json.dumps(getjson(config), indent=3)
+  cache=open(tmpfile, 'w')
+  cache.write(inventory)
+  cache.close()
 
 def printcache(tmpfile):
 	cache=open(tmpfile, 'r')
@@ -114,28 +141,15 @@ def printcache(tmpfile):
 
 
 if __name__ == '__main__':
-    tmpfile='/tmp/ansible_cache.json'
-    config = RawConfigParser()
-    config.read([os.path.dirname(os.path.abspath(__file__))+'/inventory_conf.ini'])
-    try:
-      con = mdb.connect(config.get('ansible','host'), config.get('ansible','user'), config.get('ansible','password'), config.get('ansible','db'));
-    except mdb.Error, e:
-      print "Error %d: %s" % (e.args[0],e.args[1])
-      sys.exit(1)
+  config = RawConfigParser()
+  config.read([os.path.dirname(os.path.abspath(__file__))+'/inventory_conf.ini'])
+  tmpfile=config.get('ansible','tmpfile')
 
-    if len(sys.argv) == 2 and (sys.argv[1] == '--list'):
-	if os.path.isfile(tmpfile):
-		ago=time.time()-3600
-		if os.path.getmtime(tmpfile)<ago:
-			writecache(con,tmpfile)
-	else:
-		writecache(con,tmpfile)
+  if os.path.isfile(tmpfile):
+    ago=time.time()-3600
+    if os.path.getmtime(tmpfile)<ago:
+      writecache(config,tmpfile)
+  else:
+    writecache(config,tmpfile)
 
-	printcache(tmpfile)
-    elif len(sys.argv) == 3 and (sys.argv[1] == '--host'):
-        hostinfo(con, sys.argv[2])
-    else:
-        print "Usage: %s --list or --host <hostname>" % sys.argv[0]
-        sys.exit(1)
-
-    con.close()
+  printcache(tmpfile)
